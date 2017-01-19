6e69fd9cfb88474a0d54f54815ce0916ebb75ccb
Jhonathan Wyterlin <jhonathanwyterlin@gmail.com>
2017-01-19 14:02:05 -0200

    Created more unit tests for MoviePresenter

--- PopularMovies/PopularMoviesTests/MoviePresenterTests.m
+++ PopularMovies/PopularMoviesTests/MoviePresenterTests.m
@@ -40,11 +40,38 @@ it (@"should not use an unavailable title. ", ^{
     
 });
 
-it (@"should not have title with year between parentheses. ", ^{
+it (@"should have title with year between parentheses. ", ^{
     
     MovieModel *movieModel = [[MovieModel alloc] initWithTitle:title year:year overview:overview picture:picture];
     MoviePresenter *moviePresenter = [[MoviePresenter alloc] initWithMovie:movieModel];
     expect( moviePresenter.titleYearText ).to.equal(@"Lorem Ipsum (1998)");
+    
+});
+
+it (@"should have correct overview. ", ^{
+    
+    MovieModel *movieModel = [[MovieModel alloc] initWithTitle:title year:year overview:overview picture:picture];
+    MoviePresenter *moviePresenter = [[MoviePresenter alloc] initWithMovie:movieModel];
+    expect( moviePresenter.overviewText ).to.equal(@"Curabitur molestie dolor ut sapien dictum gravida. Fusce erat leo, volutpat vel pharetra vitae, semper in sem. Proin nunc erat, dapibus ac odio sit amet, accumsan laoreet dui. In molestie erat et tellus condimentum pellentesque. Nulla risus metus, aliquam non orci sit amet, feugiat vestibulum nisi. Curabitur lorem ex, finibus sed erat nec, aliquam elementum lectus. Duis iaculis, nisi aliquam efficitur faucibus, ex lacus congue justo, in scelerisque sem nisi vitae justo. Integer id dui blandit, tincidunt enim nec, auctor eros. Donec placerat nec mauris id vehicula. Proin at efficitur ante. Suspendisse lorem arcu, pellentesque auctor tempor ac, accumsan vitae lacus. Integer vel felis eget arcu pulvinar efficitur vel quis justo. Nulla quis consectetur enim. Nulla pellentesque nulla id interdum sollicitudin.");
+    
+});
+
+it (@"should not have overview. ", ^{
+    
+    MovieModel *movieModel = [[MovieModel alloc] initWithTitle:title year:year overview:nil picture:picture];
+    MoviePresenter *moviePresenter = [[MoviePresenter alloc] initWithMovie:movieModel];
+    expect( moviePresenter.overviewText ).to.beNil;
+    
+});
+
+it (@"should have correct overview without set overviewText again. ", ^{
+    
+    MovieModel *movieModel = [[MovieModel alloc] initWithTitle:title year:year overview:overview picture:picture];
+    MoviePresenter *moviePresenter = [[MoviePresenter alloc] initWithMovie:movieModel];
+    
+    [moviePresenter overviewText];
+    
+    expect( moviePresenter.overviewText ).to.equal(@"Curabitur molestie dolor ut sapien dictum gravida. Fusce erat leo, volutpat vel pharetra vitae, semper in sem. Proin nunc erat, dapibus ac odio sit amet, accumsan laoreet dui. In molestie erat et tellus condimentum pellentesque. Nulla risus metus, aliquam non orci sit amet, feugiat vestibulum nisi. Curabitur lorem ex, finibus sed erat nec, aliquam elementum lectus. Duis iaculis, nisi aliquam efficitur faucibus, ex lacus congue justo, in scelerisque sem nisi vitae justo. Integer id dui blandit, tincidunt enim nec, auctor eros. Donec placerat nec mauris id vehicula. Proin at efficitur ante. Suspendisse lorem arcu, pellentesque auctor tempor ac, accumsan vitae lacus. Integer vel felis eget arcu pulvinar efficitur vel quis justo. Nulla quis consectetur enim. Nulla pellentesque nulla id interdum sollicitudin.");
     
 });
 
