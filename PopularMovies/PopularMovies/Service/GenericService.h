71ba35a6fa7c52dd05e316c51346f0bfcee35c57
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2017-01-19 20:52:21 -0200

    Created GenericService

---
+++ PopularMovies/PopularMovies/Service/GenericService.h
@@ -0,0 +1,13 @@
+//
+//  GenericService.h
+//  PopularMovies
+//
+//  Created by Jhonathan Wyterlin on 19/01/17.
+//  Copyright © 2017 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GenericService : NSObject
+
+@end
---
+++ PopularMovies/PopularMovies/Service/GenericService.m
@@ -0,0 +1,13 @@
+//
+//  GenericService.m
+//  PopularMovies
+//
+//  Created by Jhonathan Wyterlin on 19/01/17.
+//  Copyright © 2017 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "GenericService.h"
+
+@implementation GenericService
+
+@end
