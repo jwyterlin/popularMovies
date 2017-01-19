0e0c5157177ac5b113fcebea42fb955bfb7e3df8
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2017-01-19 20:53:27 -0200

    Created MovieService

---
+++ PopularMovies/PopularMovies/Service/MovieService.h
@@ -0,0 +1,13 @@
+//
+//  MovieService.h
+//  PopularMovies
+//
+//  Created by Jhonathan Wyterlin on 19/01/17.
+//  Copyright © 2017 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "GenericService.h"
+
+@interface MovieService : GenericService
+
+@end
---
+++ PopularMovies/PopularMovies/Service/MovieService.m
@@ -0,0 +1,13 @@
+//
+//  MovieService.m
+//  PopularMovies
+//
+//  Created by Jhonathan Wyterlin on 19/01/17.
+//  Copyright © 2017 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "MovieService.h"
+
+@implementation MovieService
+
+@end
