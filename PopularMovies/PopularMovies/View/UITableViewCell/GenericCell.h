299e1d4c0efe1e47ace004ca4f3be0545e472509
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2017-01-19 04:00:28 -0200

    Created GenericCell

---
+++ PopularMovies/PopularMovies/View/UITableViewCell/GenericCell.h
@@ -0,0 +1,13 @@
+//
+//  GenericCell.h
+//  PopularMovies
+//
+//  Created by Jhonathan Wyterlin on 19/01/17.
+//  Copyright © 2017 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GenericCell : UITableViewCell
+
+@end
---
+++ PopularMovies/PopularMovies/View/UITableViewCell/GenericCell.m
@@ -0,0 +1,24 @@
+//
+//  GenericCell.m
+//  PopularMovies
+//
+//  Created by Jhonathan Wyterlin on 19/01/17.
+//  Copyright © 2017 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "GenericCell.h"
+
+@implementation GenericCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
