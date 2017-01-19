c8d9c1445f49d84412af659bee7ce4b23480dc06
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2017-01-19 04:51:23 -0200

    Created MoviesInteractor

---
+++ PopularMovies/PopularMovies/Interactor/MoviesInteractor.m
@@ -0,0 +1,13 @@
+//
+//  MoviesInteractor.m
+//  PopularMovies
+//
+//  Created by Jhonathan Wyterlin on 19/01/17.
+//  Copyright © 2017 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "MoviesInteractor.h"
+
+@implementation MoviesInteractor
+
+@end
