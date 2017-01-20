4d99739bb93f39c3b3e5c0e5ac3930c7446f3f02
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2017-01-20 02:41:06 -0200

    Removed 'moviesMock' method from MovieService

--- PopularMovies/PopularMovies/Service/MovieService.m
+++ PopularMovies/PopularMovies/Service/MovieService.m
@@ -65,17 +65,4 @@
     
 }
 
--(NSArray<MovieModel *> *)moviesMock {
-    
-    MovieModel *movie1 = [[MovieModel alloc] initWithTitle:@"The Lord of the Rings: The Fellowship of the Ring" releaseDate:[NSDate date] overview:@"A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle Earth from the Dark Lord Sauron." posterPath:nil];
-    MovieModel *movie2 = [[MovieModel alloc] initWithTitle:@"The Matrix" releaseDate:[NSDate date] overview:@"A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers." posterPath:nil];
-    MovieModel *movie3 = [[MovieModel alloc] initWithTitle:@"Guardians of the Galaxy" releaseDate:[NSDate date] overview:@"A group of intergalactic criminals are forced to work together to stop a fanatical warrior from taking control of the universe." posterPath:nil];
-    MovieModel *movie4 = [[MovieModel alloc] initWithTitle:@"Filme" releaseDate:[NSDate date] overview:@"Sinopse." posterPath:nil];
-    
-    NSArray *movies = @[movie1,movie2,movie3,movie4];
-    
-    return movies;
-    
-}
-
 @end
